6fe72681abbd7c1186eeb72be18bcbb73150496a
zubairabd <37186480+zubairabd@users.noreply.github.com>
2025-10-28 22:07:13 +0700

    Refactor adminOnly middleware formatting

--- adminOnlyMiddleware.js
+++ adminOnlyMiddleware.js
@@ -1,12 +1,11 @@
-// File: backend/adminOnlyMiddleware.js
-
 function adminOnly(req, res, next) {
-    // Middleware ini harus dijalankan SETELAH middleware 'auth'
+
     if (req.user && req.user.role === 'admin') {
-        next(); // Lanjutkan jika user adalah admin
+        next(); 
     } else {
         res.status(403).json({ message: 'Akses ditolak. Hanya untuk Admin.' });
     }
 }
 
 module.exports = adminOnly;
+
